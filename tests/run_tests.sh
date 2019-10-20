3bb3f4ada6f00e67e4558f1362fabc756756f73c
Pedro Abreu <pdacost@purdue.edu>
2019-10-20 12:12:34 -0400

    v1: Test script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
 coqc -Q ../theories Coquedille -Q ../coq-haskell/src Hask Extraction.v
-mv main.hs.out main.hs
+
+# sed -e '/CLIENTSCRIPT="foo"/a\'$'\n''CLIENTSCRIPT2="hello"' file
+
+sed -e '/import qualified Prelude/a\'$'\n''import qualified Data.Bits' main.hs > m.hs
+sed -e '/import qualified Prelude/a\'$'\n''import qualified Data.Char' m.hs > main.hs
+rm m.hs
+
+echo "
+program_err :: Option Program0 -> Program0
+program_err None = Nil
+program_err (Some p) = p
+
+main = Prelude.putStrLn (prettyProgram (program_err (denoteCoq nat_syntax)))" >> main.hs
