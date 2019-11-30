e99a528800a7c70145013f5369f6c324e2d2f6e1
Pedro Abreu <pdacost@purdue.edu>
2019-11-29 19:49:39 -0500

    Refactor denoteTerm with monad transformers

--- theories/DenoteCoq.v
+++ theories/DenoteCoq.v
@@ -8,8 +8,11 @@ Require Import ExtLib.Structures.Applicative.
 Import MonadNotation.
 
 Require Import ExtLib.Data.Monads.StateMonad.
+Require Import ExtLib.Data.Monads.ReaderMonad.
 Require Import ExtLib.Data.Monads.OptionMonad.
 Require Import ExtLib.Data.Monads.ListMonad.
+Require Import ExtLib.Data.Monads.IdentityMonad.
+Require Import ExtLib.Data.Monads.EitherMonad.
 
 Require Import MetaCoq.Template.Ast.
 Require Import MetaCoq.Template.AstUtils.
@@ -68,57 +71,80 @@ match x with
 | nNamed name => name
 end.
 
-Section Monadic.
-  Local Open Scope monad_scope.
+Section monad.
+Local Open Scope monad_scope.
 
-  (* Definition m := state ctx. *)
-  Context {Monad_m : Monad (state ctx)}.
-  Context {State_m: MonadState Ced.Typ (state ctx)}.
+Section monadic.
 
-Fixpoint denoteTerm (t: term) (genv : global_env) {struct t}: state ctx Ced.Typ :=
+Definition m A := readerT (global_env * ctx) (eitherT string IdentityMonad.ident) A.
+Definition run_m {A} (env: global_env * ctx) (ev: m A) := unIdent (unEitherT (runReaderT ev env)).
+Context {Monad_m : Monad m}.
+Context {Reader_m: MonadReader (global_env * ctx) m}.
+Context {Either_m: MonadExc string m}.
+(* Context {Maybe_m : MonadMaybe m}. *)
+
+Definition find_err {A} l v err : string + A :=
+match nth_error l v with
+| None => raise err
+| Some x => ret x
+end.
+
+Fixpoint mapErr {A B} (l : list (A + B)) : A + list B :=
+match l with
+| nil => inr nil
+| (inl x :: xs) => inl x
+| (inr x :: xs) =>
+  xs' <- mapErr xs ;;
+  ret (x :: xs')
+end.
+
+Fixpoint denoteTerm (t: term) {struct t}: m Ced.Typ :=
 let dummyTy := Ced.TpVar "dummyTy" in
 match t with
   | tProd x t1 t2 =>
-    Γ <- get ;;
-    t1' <- ⟦ t1 ⟧ genv ;;
-    put ((binderName x) :: Γ) ;;
-    t2'  <- ⟦ t2 ⟧ genv;;
+    t1' <- ⟦ t1 ⟧ ;;
+    t2' <- local (fun '(genv, Γ) => (genv, ((binderName x) :: Γ))) ⟦ t2 ⟧;;
     ret (Ced.TpPi (DenoteName x) t1' t2')
   | tRel n =>
-    Γ <- get ;;
+    '(_, Γ) <- ask ;;
     match nth_error Γ n with
-    | None => ret (Ced.TpVar "tERR")
+    | None => raise "variable not in environment"
     | Some x => ret (Ced.TpVar x)
     end
   | tApp t1 ts2 =>
-    Γ <- get ;;
-    t1' <- ⟦ t1 ⟧ genv;;
-    let ts2' := map (fun t => evalState (⟦ t ⟧ genv) Γ) ts2 in
-    ret (Ced.TpApp t1' ts2')
+    env <- ask ;;
+    t1' <- ⟦ t1 ⟧ ;;
+    let ts2' := map (fun t => (run_m env ⟦ t ⟧)) ts2 in
+    match mapErr ts2' with
+    | inl s => raise s
+    | inr ts => ret (Ced.TpApp t1' ts)
+    end
   | tInd ind univ => ret (Ced.TpVar (kername_to_qualid (inductive_mind ind)))
   | tConstruct ind n _ =>
-    (* Can we transform this to the Maybe Monad *)
-(*        and then come back to the State Monad? *)
-    (* Perhaps use Monad Transformers *)
+    '(genv, _) <- ask ;;
+    (*     (* Can we transform this to the Maybe Monad *) *)
+    (*        and then come back to the State Monad? *)
+    (*     (* Perhaps use Monad Transformers *) *)
     match lookup_mind_decl (inductive_mind ind) genv with
-    | None => ret (Ced.TpVar "NoDecl")
+    | None => raise "Declaration not found"
     | Some d =>
       let bodies := ind_bodies d in
       match head bodies with
-      | None => ret (Ced.TpVar "NoBody")
+      | None => raise "Could not find declaration body"
       | Some body =>
         let constrs := ind_ctors body in
         match nth_error constrs n with
-        | None => ret (Ced.TpVar "NoConstr")
-        | Some x =>
-          let '(ctor, _, _) := x in ret (Ced.TpVar ctor)
+        | None => raise "Could not find constructor"
+        | Some (ctor, _, _) => ret (Ced.TpVar ctor)
         end
       end
     end
   | tSort univ => ret Ced.KdStar
-  | _ => ret (Ced.TpVar "notimpl")
+  | _ => raise "Constructor not implemented yet"
 end
 where "⟦ x ⟧" := (denoteTerm x).
+
+End monadic.
 
 Fixpoint removeBindings (t: term) (n: nat) : term :=
 match n with
@@ -192,5 +218,4 @@ match t with
 end.
 
 Local Close Scope string_scope.
-
-End Monadic.
+Local Close Scope monad_scope.
